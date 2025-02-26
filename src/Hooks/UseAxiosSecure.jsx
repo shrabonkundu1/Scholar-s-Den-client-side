e18d497aaf216bbd56442d3c39b9cc7709239652
ShrabonKundu <shrabonkundu@gmail.com>
2025-02-26 13:57:10 +0600

    All bugs remove

--- src/Hooks/UseAxiosSecure.jsx
+++ src/Hooks/UseAxiosSecure.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
-    baseURL:'http://localhost:5000'
+    baseURL:'https://scholar-s-den-server-side.vercel.app'
 })
 const useAxiosSecure = () => {
 
